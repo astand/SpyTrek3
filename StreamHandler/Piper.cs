a80c58266073cb0d715ac190e914404c8297de43
astand <quater873@gmail.com>
2016-06-24 12:33:18 +0300

    Implement reading operations

--- StreamHandler/Piper.cs
+++ StreamHandler/Piper.cs
@@ -1,54 +1,44 @@
 ﻿using StreamHandler.Abstract;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Timers;
 
 namespace StreamHandler
 {
-    public class Piper
+    public class Piper : IDisposable
     {
-        private Stream stream;
+        private MemoryStream stream;
 
-        private Timer resend_time = new Timer();
+        private System.Timers.Timer readtime;
 
         private IStreamHandler packer = new SimpleHandler();
 
-        private bool IsBusy = false;
+        public event EventHandler<PiperEventArgs> OnData;
 
-        private Int32 m_try_count;
-
-        public event DataEventHandler OnData;
-
-        public event ErrorEventHandler OnFail;
-
-        public Piper(Stream stream)
+        public event EventHandler<PiperEventArgs> OnFail;
+        
+        public Piper(MemoryStream stream)
         {
             /* need check for null ??? */
             if (stream == null)
                 throw new NullReferenceException($"Cannot assign null stream");
 
             this.stream = stream;
+            TimerForPollingStreamInit(5);
+
         }
-
-
-        public Int32 Send(IStreamData data)
-        {
-            if (IsBusy)
-                return -1;
-            IsBusy = true;
-            SendData(data);
-            return 0;
-        }
-
 
         public Int32 SendData(IStreamData streamdata)
         {
             if (!stream.CanWrite)
             {
-                OnFail(null, null);
+                OnFailCaller("Data cannot be write");
                 return -1;
             }
 
@@ -57,11 +47,54 @@ namespace StreamHandler
             return 0;
         }
 
+        public Byte[] ReadUnpackedData() => packer.GetUnpacked();
 
-
-        public void OnDataCaller()
+        public void Dispose()
         {
-            OnData(null, null);
+            readtime.Dispose();
+        }
+
+        private void TimerForPollingStreamInit(double ms)
+        {
+            readtime = new System.Timers.Timer(ms);
+            readtime.Elapsed += Readtime_Elapsed;
+            readtime.AutoReset = true;
+            readtime.Start();
+        }
+
+        private void Readtime_Elapsed(Object sender, ElapsedEventArgs e)
+        {
+            var output_buff = stream.ToArray();
+            
+            if(output_buff.Length != 0)
+            {
+                foreach (var item in output_buff)
+                {
+                    var unpack_res = packer.UnpackPacket(item);
+                    if (unpack_res)
+                    {
+                        OnDataCaller(packer.GetUnpacked(), "Packer received one packet");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void OnDataCaller(byte[] data, string message)
+        {
+            PiperEventArgs args = new PiperEventArgs(data, message);
+            Volatile.Read(ref OnData)?.Invoke(this, args);
+        }
+
+        private void OnFailCaller(string message)
+        {
+            PiperEventArgs args = new PiperEventArgs(null, message);
+            Volatile.Read(ref OnFail)?.Invoke(this, args);
+        }
+
+        public void TestOnDataInvoker()
+        {
+            OnDataCaller(new Byte[5], "Test invokation");
         }
 
     }
