cec052d8f4a0176f3c6f6abc369e41187f38e800
astand <quater873@gmail.com>
2016-06-24 12:32:10 +0300

    Added Event Data Type for Piper Events

--- StreamHandler/PiperEventHandlers.cs
+++ StreamHandler/PiperEventHandlers.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace StreamHandler
 {
-    public delegate void DataEventHandler(Object sender, EventArgs e);
+    public class PiperEventArgs : EventArgs
+    {
+        public PiperEventArgs(Byte[] data, String message = "")
+        {
+            Message = message;
+            Data = data;
+        }
 
-    public delegate void ErrorventHandler(Object sender, EventArgs e);
+        public string Message { get; set; }
+
+        public Byte[] Data { get; set; }
+
+    }
+
+    //public delegate void DataEventHandler(Object sender, EventArgs e);
+
+    //public delegate void ErrorventHandler(Object sender, EventArgs e);
 }
