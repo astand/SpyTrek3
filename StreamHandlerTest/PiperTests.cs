e84e081833bbc786b22b89f968dc948dcfb991bd
astand <quater873@gmail.com>
2016-06-24 12:33:44 +0300

    Piper Tests

--- StreamHandlerTest/PiperTests.cs
+++ StreamHandlerTest/PiperTests.cs
@@ -2,9 +2,11 @@
 using StreamHandler;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StreamHandler.Tests
@@ -27,27 +29,49 @@ namespace StreamHandler.Tests
         public void Piper_PiperCreation()
         {
             Boolean dataGet = false;
+            byte[] received_data = new byte[0];
             Piper pip = new Piper(new MemoryStream());
 
-            pip.OnData += delegate
+            pip.OnData += delegate(object obj, PiperEventArgs e)
             {
                 dataGet = true;
+                received_data = e.Data;
+                Debug.WriteLine($"Message from Event: " + e.Message);
             };
 
-            pip.OnDataCaller();
+            pip.TestOnDataInvoker();
 
             Assert.AreEqual(true, dataGet);
+            Assert.AreEqual(5, received_data.Length);
         }
 
         [TestMethod()]
         public void Piper_SendData_Test()
         {
+            Boolean received = false;
+            Byte[] received_array = null;
             StreamData streamData = new StreamData(8);
             pipe.SendData(streamData);
 
-            var length_of_writed = fifo.Length;
+            var can_read = fifo.CanRead;
 
-            Assert.AreEqual(12, length_of_writed);
+            Assert.AreEqual(true, can_read);
+            Assert.AreEqual(12, fifo.Length);
+
+            pipe.OnData += delegate(object obj, PiperEventArgs e)
+            {
+                received = true;
+                received_array = e.Data;
+            };
+
+            var count = 100;
+            /// wait some time for handling
+            while ((count-- != 0) && (received == false))
+                Thread.Sleep(10);
+
+            Assert.AreEqual(true, received);
+            Assert.AreEqual(8, received_array.Length);
+            //SimpleHandlerTests.PrintArray(fifo.ToArray());
         }
     }
 }
