457624deef6af1cf87d646092c3aff1b022f5d59
astand <quater873@gmail.com>
2016-06-26 11:35:12 +0300

    Working only with IPipeReadable objects

--- StreamHandler/Abstract/IStreamHandler.cs
+++ StreamHandler/Abstract/IStreamHandler.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using StreamHandler.Abstract;
 
 namespace StreamHandler
 {
@@ -17,12 +19,14 @@ namespace StreamHandler
         /// </summary>
         /// <param name="packedbuf"></param>
         /// <returns>return true if good packet recived</returns>
-        bool UnpackPacket(byte[] packedbuf);
+        //bool UnpackPacket(byte[] packedbuf);
 
-        bool UnpackPacket(Stream stream);
+        //bool UnpackPacket(Stream stream);
 
-        bool UnpackPacket(Byte bt);
+        bool ExtractPacket(IPipeReadable reader);
 
-        byte[] GetUnpacked();
+        //bool UnpackPacket(Byte bt);
+
+        byte[] Data();
     }
 }
