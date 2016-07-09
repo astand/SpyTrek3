e0be171128ea55cd45d2630c926383dacc036fb5
astand <quater873@gmail.com>
2016-07-09 12:19:49 +0300

    Added COR template

---
+++ MessageHandler/FrameHandler/IHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.Abstract
+{
+    /// <summary>
+    /// Base class for chain of responsibility. Objects of this class 
+    /// interlock for handling appropriated PacketFrames
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHandler<T>
+    {
+        void SetSuccessor(IHandler<T> handler);
+        void HandleRequest(T o, UInt16 fid);
+        void SetSpecification(Func<UInt16, bool> spec);
+    }
+}
