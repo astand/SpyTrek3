0d90da3ffb29faaab8e8f9db6da37ce46a1518b9
astand <quater873@gmail.com>
2016-07-09 15:40:59 +0300

    Added TrekDescription type

---
+++ MessageHandler/DataFormats/TrekDescriptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.DataFormats
+{
+    class TrekDescriptor
+    {
+        private UInt16 id;
+
+        private DateTime start;
+
+        private DateTime stop;
+
+        public UInt32 TrekSize { get; private set; }
+        public UInt32 Dist { get; set; }
+        public UInt32 Odometr { get; set; }
+
+
+
+        private static readonly Int32 MINIMUM_DISTANCE_DETECT = (200 * 10);
+    };
+
+}
+
