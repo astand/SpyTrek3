3c974aee1848501bd4d5c542169ed53e62c8774f
astand <quater873@gmail.com>
2016-07-09 12:20:44 +0300

    Added Handler for concrete files

---
+++ MessageHandler/FrameHandler/FileHandler.cs
@@ -0,0 +1,63 @@
+﻿using MessageHandler.Abstract;
+using StreamHandler;
+using StreamHandler.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.ConcreteHandlers
+{
+    public class FileHandler<T> : IHandler<T> where T : FramePacket
+    {
+        private IHandler<T> m_successor;
+
+        private string name;
+
+        private Func<UInt16, bool> CheckFileID;
+
+        private IFrameProccesor processor;
+
+        private IStreamData m_answer;
+
+        private Func<IStreamData, int> sending;
+
+        public FileHandler(string name,
+            IFrameProccesor processor,
+            Func<IStreamData, int> pipe)
+        {
+            this.name = name;
+            this.processor = processor;
+            sending = pipe;
+        }
+
+        public void HandleRequest(T o, UInt16 id)
+        {
+            if (CheckFileID(id))
+            {
+                processor?.Process(o, ref m_answer);
+                if (m_answer != null)
+                {
+                    sending(m_answer);
+                }
+            }
+            else
+                m_successor?.HandleRequest(o, id);
+
+
+        }
+
+        public void SetSpecification(Func<UInt16, Boolean> spec)
+        {
+            CheckFileID = spec;
+        }
+
+        public void SetSuccessor(IHandler<T> successor)
+        {
+            m_successor = successor;
+        }
+
+    }
+}
