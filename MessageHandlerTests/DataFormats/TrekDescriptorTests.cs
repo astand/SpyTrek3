54171be162547fe32d719a5c7e412ff8e7d51c24
astand <quater873@gmail.com>
2016-07-09 17:01:14 +0300

    Added Test for TrekDescription parsing

---
+++ MessageHandlerTests/DataFormats/TrekDescriptorTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessageHandler.DataFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MessageHandler.DataFormats.Tests
+{
+    [TestClass()]
+    public class TrekDescriptorTests
+    {
+        Byte[] array_ok = new byte[] {
+            1, 0,
+            12, 1, 21, 11, 11, 12,
+            12, 1, 21, 11, 23, 02,
+            12, 2, 0, 0,
+            3, 1, 2, 0,
+            10, 20, 0, 0
+        };
+
+        Byte[] array_short = new byte[] {
+            1, 0,
+            12, 1, 21, 11, 11, 12,
+            12, 1, 21, 11, 23, 02,
+            12, 2, 0, 0,
+            3, 1, 2, 0,
+            10, 20
+        };
+
+
+        [TestInitialize()]
+        public void Init()
+        {
+
+        }
+
+        [TestMethod()]
+        public void TrekDescriptor_Constructor_Ok()
+        {
+            var desc = new TrekDescriptor();
+            
+            var ret = desc.TryParse(array_ok, 0);
+            Assert.AreEqual(true, ret);
+        }
+
+        [TestMethod()]
+        public void TrekDescriptor_Constructor_Bad()
+        {
+            var desc = new TrekDescriptor();
+
+            var ret = desc.TryParse(array_short, 0);
+            Assert.AreEqual(false, ret);
+        }
+    }
+}
