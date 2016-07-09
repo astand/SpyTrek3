4923147b0b817950943ccd40270522860d202362
astand <quater873@gmail.com>
2016-07-09 12:26:53 +0300

    Added interface that uses for process received frames in file handlers

---
+++ MessageHandler/Frame/IFrameProcessor.cs
@@ -0,0 +1,22 @@
+﻿using StreamHandler.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler
+{
+
+    /// <summary>
+    /// IFrameProcessor uses for invoke action in Read, Write or any
+    /// other custom IHandlers
+    /// </summary>
+    public interface IFrameProccesor
+    {
+        void Process(FramePacket packet, ref IStreamData answer);
+    }
+
+   
+    
+}
