dad59ecf6e361ad73b63f18d6e4304a7e8c3e881
astand <quater873@gmail.com>
2016-07-09 14:58:34 +0300

    Added utility for DateTime simple working

---
+++ MessageHandler/Extension/DateTimeUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.DataFormats
+{
+
+    public static class DateTimeUtil
+    {
+        /// <summary>
+        /// Create DateTime instance from packed to byte array
+        /// format [year, month, day, hour, min, sec]
+        /// </summary>
+        /// <param name="buff"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public static DateTime GetDateTime(byte[] buff, int offset = 0)
+        {
+            DateTime ret = new DateTime(
+                buff[offset] + 2000,
+                buff[offset + 1],
+                buff[offset + 2],
+                buff[offset + 3],
+                buff[offset + 4],
+                buff[offset + 5]);
+
+            return ret;
+        }
+
+        public static string ToDirectory(this DateTime dt) => $"{dt.Year:D4}/{dt.Month:D2}/{dt.Day:D2}";
+
+        public static string ToJS(this DateTime dt) => dt.ToLocalTime().ToString(@"yyyy.MM.dd HH:mm");
+    }
+}
---
+++ MessageHandlerTests/DataFormats/DateTimeUtilTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessageHandler.DataFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MessageHandler.DataFormats.Tests
+{
+    [TestClass()]
+    public class DateTimeUtilTests
+    {
+        Byte[] short_array = { 10, 4, 4, 12, 12 };
+        Byte[] wrong_param_array = { 10, 4, 4, 99, 90, 00 };
+        Byte[] okdate = { 10, 4, 4, 12, 12, 00 };
+
+        DateTime victim;
+        [TestInitialize()]
+        public void Init()
+        {
+            victim = DateTimeUtil.GetDateTime(okdate);
+        }
+
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [TestMethod()]
+        public void DateTimeFromByteArray_ShortArray()
+        {
+            DateTime dt = DateTimeUtil.GetDateTime(short_array, 0);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod()]
+        public void DateTimeFromByteArray_WrongArray()
+        {
+            DateTime dt2 = DateTimeUtil.GetDateTime(wrong_param_array, 0);
+        }
+
+        [TestMethod()]
+        public void DateTimeFromByteArray_Ok()
+        {
+            DateTime dt2 = DateTimeUtil.GetDateTime(okdate, 0);
+        }
+
+
+
+        [TestMethod()]
+        public void DateTimeFromByteArray_ToFrmString_Test()
+        {
+            Assert.AreEqual(2010, victim.Year);
+            Debug.WriteLine(victim.ToDirectory());
+            Debug.WriteLine(victim.ToJS());
+        }
+    }
+}
