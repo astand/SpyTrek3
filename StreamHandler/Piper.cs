7616c1a0b5cfebace58ab0e3ed90e1a95d00ad10
astand <quater873@gmail.com>
2016-07-28 13:10:32 +0300

    Revert "Using Queue for caching outcome packets"
    This reverts commit efd86ee6fc91a44e03d11ed91ba41e0697a34f3a.

--- StreamHandler/Piper.cs
+++ StreamHandler/Piper.cs
@@ -26,9 +26,7 @@ namespace StreamHandler
         public event EventHandler<PiperEventArgs> OnData;
 
         public event EventHandler<PiperEventArgs> OnFail;
-
-        private Queue<Byte> from_buff = new Queue<byte>(8192);
-
+        
         public Piper(/*Stream stream, */IPipeReader reader, IPipeWriter writer)
         {
             /* need check for null ??? */
@@ -46,11 +44,7 @@ namespace StreamHandler
         public Int32 SendData(IStreamData streamdata)
         {
             var array_to_send = packman.PackPacket(streamdata.SerializeToByteArray());
-            foreach (var b in array_to_send)
-                from_buff.Enqueue(b);
-            Console.WriteLine($"Cached data for sending. Current length = {from_buff.Count}");
-
-            //writer.Write(array_to_send, array_to_send.Length);
+            writer.Write(array_to_send, array_to_send.Length);
             return 0;
         }
 
@@ -71,17 +65,8 @@ namespace StreamHandler
 
         private void Readtime_Elapsed(Object sender, ElapsedEventArgs e)
         {
-            if (from_buff.Count != 0)
-            {
-                var array = new Byte[from_buff.Count];
-                from_buff.CopyTo(array, 0);
-                from_buff.Clear();
-                writer.Write(array);
-                Console.WriteLine($"Data was sent. Sent length = {array.Length}");
-                Console.WriteLine($"ID = {BitConverter.ToUInt16(array, 3)}");
-            }
-
             if (packman.ExtractPacket(reader))
+            //if (packer.UnpackPacket(reader))
             {
                 OnDataCaller(packman.Data(), "Packer received one packet");
             }
