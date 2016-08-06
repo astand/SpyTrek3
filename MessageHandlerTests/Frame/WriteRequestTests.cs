b39fa5a136fd0caddd922282ef0f06cb721e6177
astand <quater873@gmail.com>
2016-08-06 10:48:06 +0300

    Write request added

---
+++ MessageHandlerTests/Frame/WriteRequestTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessageHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.Tests
+{
+    [TestClass()]
+    public class WriteRequestTests
+    {
+        [TestMethod()]
+        public void WriteRequest_WriteRequest_Test()
+        {
+            var victim = new WriteRequest(1, 20000);
+
+            var retarray = victim.SerializeToByteArray();
+
+
+            Assert.AreEqual(10, retarray.Length);
+        }
+    }
+}
