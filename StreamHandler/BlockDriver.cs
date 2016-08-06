c41da0f82c90f111c1fe8d4f6321af032f5a67dc
astand <quater873@gmail.com>
2016-08-06 10:47:39 +0300

    Block driver added

---
+++ StreamHandler/BlockDriver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamHandler
+{
+    public class BlockDriver
+    {
+        public UInt16 BidSend { get; set; }
+
+        public UInt16 BidAck { get; set; } = 0;
+
+        public UInt16 BidLast { get; set; }
+
+        private readonly int m_resend_count = 2;
+
+        private readonly int m_bid_window = 8;
+
+        public BlockDriver() { }
+
+        public bool PassAckBlock(UInt16 ack_block_id)
+        {
+            if (ack_block_id == BidAck + 1)
+            {
+                BidAck += 1;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsLast => BidLast == BidSend;
+
+        public bool IsLastAck => BidAck == BidLast;
+
+        public bool IsWindAllow() => ((BidAck + m_bid_window) > BidSend) && !IsLast;
+
+        public void Reset()
+        {
+            BidLast = BidAck = 0;
+            BidSend = 1;
+        }
+    }
+}
---
+++ StreamHandlerTest/BlockDriverTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StreamHandler;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamHandler.Tests
+{
+    [TestClass()]
+    public class BlockDriverTests
+    {
+        [TestMethod()]
+        public void BlockDriver_PassAckBlock_Test()
+        {
+            BlockDriver blockDriver = new BlockDriver();
+
+            UInt16[] bids = { 0, 1, 2, 3, 4, 5, 6};
+
+            foreach(var bid in bids)
+            {
+                bool ret = blockDriver.PassAckBlock(bid);
+                Debug.WriteLine($"[{bid}] : ret = {ret}");
+                Assert.AreEqual((bid == 0) ? false : true, ret);
+
+            }
+        }
+    }
+}
