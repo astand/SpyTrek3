6c14603e94a80c25fb1457fdc98c32292cdd042c
astand <quater873@gmail.com>
2016-09-29 15:58:57 +0300

    added ListNodes collection.

---
+++ SpyTrekHost/UserUI/ListNodes.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SpyTrekHost.UserUI
+{
+    partial class ListNodes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(260, 25);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Num});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 11;
+            this.dataGridView1.Size = new System.Drawing.Size(418, 392);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Num
+            // 
+            this.Num.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Num.HeaderText = "#";
+            this.Num.Name = "Num";
+            this.Num.ReadOnly = true;
+            // 
+            // ListNodes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 469);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "ListNodes";
+            this.Text = "ListNodes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+    }
+}
---
+++ SpyTrekHost/UserUI/ListNodesCode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpyTrekHost.UserUI
+{
+    public partial class ListNodes : Form
+    {
+        protected Func<List<HandleInstance>> delGetList;
+
+        public void SetListGetter(Func<List<HandleInstance>> d)
+        {
+            delGetList = d;
+        }
+
+
+
+
+        private void btnRefresh_Click(Object sender, EventArgs e)
+        {
+            UpdateListNodes();
+        }
+
+        public void EnforceUpdating()
+        {
+            if (InvokeRequired)
+            {
+                Action del = new Action(UpdateListNodes);
+                Invoke(del, new object[] { });
+            }
+            else
+            {
+                UpdateListNodes();
+            }
+        }
+
+        private void UpdateListNodes()
+        {
+            dataGridView1.Rows.Clear();
+
+            var list = delGetList?.Invoke();
+
+            foreach (var item in list)
+            {
+                dataGridView1.Rows.Add(item.ToString());
+            }
+        }
+    }
+}
