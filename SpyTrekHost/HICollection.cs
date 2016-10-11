9642aa1ef812f1bbed17d92d555672f3ac1ba3d1
astand <quater873@gmail.com>
2016-10-11 20:45:11 +0300

    Added static HI collection class.

---
+++ SpyTrekHost/HICollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SpyTrekHost
+{
+    public static class HICollection
+    {
+        static List<HandleInstance> list_;
+        static public List<HandleInstance> List => list_;
+        static HICollection()
+        {
+            list_ = new List<HandleInstance>();
+        }
+
+        public static void Add(HandleInstance instance)
+        {
+            lock (list_)
+            {
+                instance.SelfDeleter += Deleter;
+                list_.Add(instance);
+            }
+        }
+
+        public static void Remove(HandleInstance instance)
+        {
+            lock (list_)
+            {
+                instance.SelfDeleter -= Deleter;
+                list_.Remove(instance);
+            }
+        }
+
+
+        public static void Deleter(Object sender, EventArgs e)
+        {
+            lock (list_)
+            {
+                var item = sender as HandleInstance;
+                Int32 index = list_.IndexOf(item);
+                if (index >= 0)
+                    list_.RemoveAt(index);
+
+                item.Dispose();
+                Debug.WriteLine($"Item [{index}] was been deleted.");
+            }
+        }
+
+        public static HandleInstance GetByIndex(int index)
+        {
+            if (index < list_.Count)
+                return list_[index];
+            return null;
+        }
+    }
+}
