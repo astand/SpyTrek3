2a68eee57312c03be9ff69a2ccd4de7cde3f5949
astand <quater873@gmail.com>
2016-10-11 21:06:09 +0300

    Added event for TrekNotes list updating.

--- SpyTrekHost/UserUI/OneNodeCode.cs
+++ SpyTrekHost/UserUI/OneNodeCode.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MessageHandler.DataFormats;
+using MessageHandler.Notifiers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,10 +10,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace SpyTrekHost.UserUI
 {
     public partial class OneNodeForm : Form
     {
-
+        public void AddNotesToGridView(Object obj, NoteListEventArgs e)
+        {
+            foreach (var item in e.NoteList)
+            {
+                float indist = 0;
+                indist = (item.Dist / 10000);
+                UInt32 allmileage = (item.Odometr/ 10000);
+                dataGridView1.Rows.Add(item.Id, item.ToString(), item.TrekSize, indist, allmileage);
+            }
+        }
     }
 }
