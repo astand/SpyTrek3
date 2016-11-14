4a66537bcda8af5ae190b071f201280e2c0ab5df
astand <quater873@gmail.com>
2016-11-14 21:28:30 +0300

    Added special for BlockID control.

---
+++ MessageHandler/Processors/BidControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.Processors
+{
+    public class BidControl
+    {
+        private Int32 bid;
+        public void Reset() => bid = 0;
+
+        public bool Next(Int32 nbid)
+        {
+            if (nbid == bid + 1)
+            {
+                bid += 1;
+                return true;
+            }
+
+            return (false);
+        }
+
+    }
+}
