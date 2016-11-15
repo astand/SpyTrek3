1083423d0a789cd0eebc7af39b2b3ec7abd77555
astand <quater873@gmail.com>
2016-11-15 09:41:03 +0300

    New error processor was added.

---
+++ MessageHandler/Processors/ErrorProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreamHandler.Abstract;
+
+namespace MessageHandler.Processors
+{
+    public class ErrorProcessor : IFrameProccesor
+    {
+        public override void Process(FramePacket packet, ref IStreamData answer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
